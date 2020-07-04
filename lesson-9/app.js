7df373d5e2921672d5761e624f14fe30fb57c879
teleskop 150750 <teleskop150750@gmail.com>
2020-07-04 22:07:13 +0300

    Задание 9

--- lesson-9/app.js
+++ lesson-9/app.js
@@ -37,3 +37,20 @@ user.plan.basic = 'basic';
 
 console.log(value);
 console.log(user);
+
+
+// Задание
+// 1. Создать объект с полем product, равным ‘iphone’
+const obj = {
+  product: 'iphone',
+};
+// 2. Добавить в объект поле price, равное 1000 и поле currency, равное ‘dollar’
+obj.prise = 1000;
+obj.currency = 'dollar';
+// 3. Добавить поле details, которое будет содержать объект с полями model и color
+obj.details = {
+  model: null,
+  color: null,
+};
+
+console.log(obj);
