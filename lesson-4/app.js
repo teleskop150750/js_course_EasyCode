af2367e2fb724709d9f480030a6863ae3acf28b3
teleskop 150750 <teleskop150750@gmail.com>
2020-07-04 18:58:53 +0300

    Урок 4

---
+++ lesson-4/app.js
@@ -0,0 +1,51 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-vars */
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
+// var, let, const
+var name = 'Denis';
+console.log(name);
+name = 'Den';
+console.log(name);
+
+var age;
+console.log(age);
+age = 30;
+console.log(age);
+
+var $name = 'Ivan';
+var _name = 'Maks';
+// var 1name = '';
+
+var userAge = 30;
+var user_age = 20;
+var UserAge = 40;
+var userage = 10;
+
+var car;
+console.log(car);
+car = 'bmw';
+// var car = 'audi';
+console.log(car);
+console.clear();
+
+// console.log(nickname);
+let nickname = 'dmgame';
+nickname = 'dm';
+console.log(nickname);
+
+const firstName = 'Tom';
+// firstName = 'Jhon';
+// const lastName;
+
+console.log(firstName);
+
+const user = {
+  name: 'Denis',
+  age: 30,
+};
+
+user.age = 25;
+
+console.log(user);
