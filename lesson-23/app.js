270e3a72834c7cc98de7f4c16c307b26f4d50811
teleskop 150750 <teleskop150750@gmail.com>
2020-08-02 22:28:23 +0300

    урок 23

---
+++ lesson-23/app.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-new-wrappers */
+const str = 'Hello world';
+// console.dir(String('hello').slice(1, -1));
+const strObj = new String('hello');
+console.log(strObj);
+// console.dir(Number);
